20f0eef1ee470a864668383b181b4c87de51a774
Sardor Salimov <112092504+sardor-prgmr@users.noreply.github.com>
2022-12-28 13:12:58 +0500

    Add files via upload

---
+++ #23 MODULLAR.py
@@ -0,0 +1,63 @@
+# import avto_info_mod
+
+# avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# avto_info_mod.info_print(avto1)
+
+# import avto_info_mod as aim
+
+# avto1 = aim.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# aim.info_print(avto1)
+
+# from avto_info_mod import avto_info, info_print
+
+# avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# info_print(avto1)
+
+# from avto_info_mod import avto_info as ainfo, info_print as iprint
+
+# avto1 = ainfo("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# iprint(avto1)
+
+# from avto_info_mod import *
+
+# avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+# info_print(avto1)
+
+# avtolar = avtoil.avto_kirit()
+
+# for avto in avtolar:
+#     avtoil.info_print(avto)
+
+
+
+import math
+
+x = 400
+# print(math.sqrt(x))
+# print(math.pow(6,2))
+# print(math.pi)
+print(math.log2(8))
+print(math.log10(100))
+
+
+
+import random as r
+
+# randint()
+# son = r.randint(10,50)
+# print(son)
+
+# choice()
+# ismlar = ['olim','anvar','hasan','husan']
+# ism = r.choice(ismlar)
+# print(ism)
+# print(r.choice(ism))
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+# shuffle()
+x = list(range(11))
+print(x)
+r.shuffle(x)
+print(x)
---
+++ #24 lambda.py
@@ -0,0 +1,19 @@
+import math
+
+# def nom(argument):
+# return ifoda
+
+# uzunlik = lambda pi, r : 2*pi*r
+# print(uzunlik(math.pi,10))
+
+# kvadrat = lambda x, y : x ** y
+# print(kvadrat(3, 2))
+
+
+def daraja(n):
+    return lambda x: x ** n
+
+
+kvadrat = daraja(2)
+kub = daraja(3)
+print(f"3-ning kvadrati {kvadrat(3)} ga, " f"kubi {kub(3)} ga teng")
